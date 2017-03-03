e46115873845a3397ae26f8c52774dc706d76b33
sebastian <none>
2017-03-03 10:26:45 +0100

    Test connectivity with other microservices

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 'use strict';
 
-var Primus = require('primus');
+const Primus = require('primus');
+const jwt = require('jsonwebtoken');
+const unirest = require('unirest');
 
-var primus = Primus.createServer(function connection(spark) {
+// need to be cleaned up
+const loggedInUsers = {
+};
+
+const primus = Primus.createServer(function connection(spark) {
     spark.on('data', function(data) {
-        spark.write('thank you for the data');
+        if (data.token) {
+            // check if existing and still valid
+            // otherwise we should end the connection here
+            spark.user = loggedInUsers[data.token];
+        }
+        if (data.action == 'doStuff') {
+            console.log(data.action, spark.user);
+            spark.write({action: 'hello '+data.action});
+        }
     });
     console.log('client connected');
 }, { port: 8080, transformer: 'websockets' });
 
 primus.authorize(function (req, done) {
-    if (req.query.token != 'acdc') {
-        return done('wrong token!');
-    }
-    done();
+    unirest.get('http://accreditor.spielstand.net/publicKey')
+        .end(function (response, foo) {
+            try {
+                const validatedPayload = jwt.verify(req.query.token, response.body);
+                if (!validatedPayload.userId) {
+                    return done({statusCode: 403, message: 'unauthorized'});
+                }
+                console.log(validatedPayload.userId, 'connected');
+                loggedInUsers[req.query.token] = validatedPayload;
+                done();
+            } catch(err) {
+                console.log(err);
+                done({statusCode: 403, message: 'unauthorized'});
+            }
+        });
 });
--- test.js
+++ test.js
@@ -4,10 +4,34 @@ var Primus = require('primus');
 
 var Socket = Primus.createSocket({});
 
-var client = new Socket('http://localhost:8080?token=acdc');
-client.on('open', function (server) {
-    client.on('data', function(data) {
-        console.log(data);
+const connect = function(token) {
+    var client = new Socket('http://localhost:8080?token='+token);
+
+    client.on('error', function (err) {
+        // try to handle auth failed
+        // console.log(err);
     });
-    client.write({ foo: 'bar' });
-});
+
+    client.on('open', function (server) {
+        client.write({token: token});
+        client.on('data', function(data) {
+            console.log(data.action);
+        });
+        client.write({action: 'doStuff'});
+    });
+};
+
+const unirest = require('unirest');
+const config = require('./config.json');
+
+unirest.post('http://accreditor.spielstand.net/generate')
+    .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+    .auth({
+      user: config.username,
+      pass: config.password,
+      sendImmediately: true
+    })
+    .send({ "userId": "U123" })
+    .end(function (response) {
+        connect(response.body);
+    });
